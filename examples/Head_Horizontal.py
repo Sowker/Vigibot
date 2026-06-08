a4438f64601418786f066ccb8dd7e2238cfb95fe
Sowker <remi.pyt@gmail.com>
2026-06-08 11:36:46 +0200

    Head test

--- examples/Head_Horizontal.py
+++ examples/Head_Horizontal.py
@@ -12,23 +12,23 @@ pca.frequency = 50
 
 # The pulse range is 750 - 2250 by default. This range typically gives 135 degrees of
 # range, but the default is to use 180 degrees. You can specify the expected range if you wish:
-class servo_head(ID):
-    def __init__(self):
+class servo_head():
+    def __init__(self,ID):
         self.servo = servo.Servo(pca.channels[ID], min_pulse=500, max_pulse=2400,actuation_range=135)
         servo.angle = 180
         self.angle = servo.angle
 
-    def set_angle(angle):
+    def set_angle(self, angle):
         self.angle = angle
 
-    def right():
+    def right(self):
         for i in range(180):
-            set_angle(channel, 180)
+            self.set_angle(180)
             time.sleep(0.01)
 
-    def left():
+    def left(self):
         for i in range(180):
-            set_angle(channel, 180 - i)
+            self.set_angle(180 - i)
             time.sleep(0.01)
 
     def test(self):
