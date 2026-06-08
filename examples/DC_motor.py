5b8275f31c94d3a2d472fb811f06d33fbced2b39
PierreMo <pierremouries05@gmail.com>
2026-06-08 13:47:35 +0200

    DC_motor control

--- examples/DC_motor.py
+++ examples/DC_motor.py
@@ -4,93 +4,107 @@ from board import SCL, SDA
 import busio
 from adafruit_pca9685 import PCA9685
 from adafruit_motor import motor
+import keyboard
 
 # motor_EN_A: Pin7  |  motor_EN_B: Pin11
 # motor_A:  Pin8,Pin10    |  motor_B: Pin13,Pin12
 
 
-MOTOR_M1_IN1 = 15  # Define the positive pole of M1
-MOTOR_M1_IN2 = 14  # Define the negative pole of M1
-MOTOR_M2_IN1 = 12  # Define the positive pole of M2
-MOTOR_M2_IN2 = 13  # Define the negative pole of M2
-MOTOR_M3_IN1 = 11  # Define the positive pole of M3
-MOTOR_M3_IN2 = 10  # Define the negative pole of M3
-MOTOR_M4_IN1 = 8  # Define the positive pole of M4
-MOTOR_M4_IN2 = 9  # Define the negative pole of M4
+class DC_Motor():
+    def __init__(self):
+        self.MOTOR_M1_IN1 = 15  # Define the positive pole of M1
+        self.MOTOR_M1_IN2 = 14  # Define the negative pole of M1
+        # def setup():
+        self.i2c = busio.I2C(SCL, SDA)
+        # Create a simple PCA9685 class instance.
+        #  pwm_motor.channels[7].duty_cycle = 0xFFFF
+        self.pwm_motor = PCA9685(i2c, address=0x5f)  # default 0x40
+        self.pwm_motor.frequency = 50
 
+        self.motor1 = motor.DCMotor(pwm_motor.channels[MOTOR_M1_IN1], pwm_motor.channels[MOTOR_M1_IN2])
+        self.motor1.decay_mode = (motor.SLOW_DECAY)
 
-def map(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) / (in_max - in_min) * (out_max - out_min) + out_min
+        self.stopped = 1
+        self.motorStop()
 
+    def motorStop():  # Motor stops
+        motor1.throttle = 0
+        self.stopped = 1
 
-# def setup():
-i2c = busio.I2C(SCL, SDA)
-# Create a simple PCA9685 class instance.
-#  pwm_motor.channels[7].duty_cycle = 0xFFFF
-pwm_motor = PCA9685(i2c, address=0x5f)  # default 0x40
-pwm_motor.frequency = 50
+    def _map(x, in_min, in_max, out_min, out_max):
+        return (x - in_min) / (in_max - in_min) * (out_max - out_min) + out_min
 
-motor1 = motor.DCMotor(pwm_motor.channels[MOTOR_M1_IN1], pwm_motor.channels[MOTOR_M1_IN2])
-motor1.decay_mode = (motor.SLOW_DECAY)
-motor2 = motor.DCMotor(pwm_motor.channels[MOTOR_M2_IN1], pwm_motor.channels[MOTOR_M2_IN2])
-motor2.decay_mode = (motor.SLOW_DECAY)
-motor3 = motor.DCMotor(pwm_motor.channels[MOTOR_M3_IN1], pwm_motor.channels[MOTOR_M3_IN2])
-motor3.decay_mode = (motor.SLOW_DECAY)
-motor4 = motor.DCMotor(pwm_motor.channels[MOTOR_M4_IN1], pwm_motor.channels[MOTOR_M4_IN2])
-motor4.decay_mode = (motor.SLOW_DECAY)
+    def destroy():
+        motorStop()
+        pwm_motor.deinit()
 
-
-#  motorStop()
-
-def Motor(channel, direction, motor_speed):
-    if motor_speed > 100:
-        motor_speed = 100
-    elif motor_speed < 0:
-        motor_speed = 0
-    speed = map(motor_speed, 0, 100, 0, 1.0)
-    if direction == -1:
-        speed = -speed
-
-    if channel == 1:
+    def _power(direction, motor_speed):
+        '''
+        Power the motor
+        '''
+        if motor_speed > 100:
+            motor_speed = 100
+        elif motor_speed < 0:
+            motor_speed = 0
+        speed = map(motor_speed, 0, 100, 0, 1.0)
+        if direction == -1:
+            speed = -speed
         motor1.throttle = speed
-        # print("1111")
-    elif channel == 2:
-        motor2.throttle = speed
-        # print("2222")
-    elif channel == 3:
-        motor3.throttle = speed
-        # print("3333")
-    elif channel == 4:
-        motor4.throttle = speed
-        # print("4444")
+
+    def _ramp(self, stop_time, direction, speed):
+        '''
+        Slowly accelerate
+        '''
+        if stop_time > 1:
+            stop_time = 1
+        step = speed / (stop_time*10)
+        actual_speed = 0
+        for i in range(0, stop_time*10):
+            actual_speed += step
+            _power(direction, actual_speed)
+            time.sleep(0.1)
+
+    def control(self, direction, motor_speed, duration=1, ramp=1, slow=0):
+        '''
+        Command to move forward or backward, movement won't stop until stopped (DC_Motor.motoStop())
+        '''
+        if self.stopped:
+            self.stopped = 0
+        # if slow mode, no ramp and 1/4 power (25)
+        if slow == 1:
+            _power(direction, 25)
+        # else ramp
+        else:
+            if duration < 1:
+                self._ramp(duration, direction, motor_speed)
+                self.motorStop()
+            else:
+                self._ramp(1, direction, motor_speed)
+
+    # def run(self, ramp=1, slow=0):
+    #     '''
+    #     Run by reacting to the keyboard
+    #     '''
+    #     while True:
+    #         if keyboard.is_pressed('z'):
+    #             print("You pressed 'q'")
+    #             break
+    #         time.sleep(0.1)
 
 
-def motorStop():  # Motor stops
-    motor1.throttle = 0
-    motor2.throttle = 0
-    motor3.throttle = 0
-    motor4.throttle = 0
-
-
-def destroy():
-    motorStop()
-    pwm_motor.deinit()
 
 
 if __name__ == '__main__':
     try:
-        chann = 1
-        # while True:
-
+        motor = DC_Motor()
         for i in range(10):
-            speed_set = 10
-            Motor(chann, 1, speed_set)
+            motor.control(1, 100)
 
             print("Forward")
-            time.sleep(2)
-            Motor(chann, -1, speed_set)
+            time.sleep(3)
+            motor.control(-1, 100)
             print("Backward")
-            time.sleep(2)
+            time.sleep(3)
         destroy()
     except KeyboardInterrupt:
         destroy()
