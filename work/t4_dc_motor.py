6cb952d59f67d4f95aec8579d967a6de2c9f7144
PierreMo <pierremouries05@gmail.com>
2026-06-10 13:50:22 +0200

    normal speed 10

--- work/t4_dc_motor.py
+++ work/t4_dc_motor.py
@@ -15,9 +15,9 @@ MOTOR_RAMP_STEPS      = 10   # nombre de pas par seconde de rampe
 CHANNEL_MOTOR_IN1        = 15  # Moteur DC pôle +
 CHANNEL_MOTOR_IN2        = 14  # Moteur DC pôle −
 
-SPEED_NORMAL_PCT      = 30   # % puissance — ligne droite
-SPEED_TURNING_PCT     = 20   # % puissance — virage doux
-SPEED_SLOW_PCT        = 10   # % puissance — virage serré
+SPEED_NORMAL_PCT      = 10   # % puissance — ligne droite
+SPEED_TURNING_PCT     = 5   # % puissance — virage doux
+SPEED_SLOW_PCT        = 3   # % puissance — virage serré
 
 class Direction(IntEnum):
     """Sens de marche du moteur DC."""
