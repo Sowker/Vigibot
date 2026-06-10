2b6f7409efaea64e0615c82eac0c4e93a26fab5f
Sowker <remi.pyt@gmail.com>
2026-06-10 14:37:21 +0200

    Pin Pon

--- work/buzzer_Sirene.py
+++ work/buzzer_Sirene.py
@@ -6,11 +6,31 @@ from time import sleep
 tb = TonalBuzzer(18) 
 
 # Define a musical tune as a sequence of notes and durations.
-SONG =	[
+POLICE =	[
     ["A5", 0.4], ["D5", 0.4],
     ["A5", 0.4], ["D5", 0.4],
     ["A5", 0.4], ["D5", 0.4],
     ["A5", 0.4], ["D5", 0.4]
+]
+MII = [
+    # 1ère phrase : tut, tut, tut, tut, tut... tut tut tuuut !
+    ["F#4", 0.25],[None, 0.25], ["A4", 0.25], ["C#5", 0.25], [None, 0.25],
+    ["A4", 0.25],[None, 0.25],  ["F#4", 0.25], ["D4", 0.15],[None, 0.1],["D4", 0.15],
+    [None, 0.1],["D4", 0.15],[None, 0.1],
+    [None, 1],
+
+    # 2ème phrase
+    ["C#4", 0.25], ["D4", 0.25], ["F#4", 0.25], ["A4", 0.25],
+    ["C#5", 0.125],[None, 0.125] ["A4", 0.2], ["F#4", 0.2], ["E5", 0.4],
+    ["D#5", 0.6],
+    [None, 0.2],
+
+    # 3ème phrase (la grande descente)
+    ["D4", 0.2], ["G#4", 0.2], ["C#5", 0.2], ["F#5", 0.2],
+    ["C#5", 0.2], ["G#4", 0.2], ["C#5", 0.2], ["G4", 0.2],
+    ["F#4", 0.2], ["C#5", 0.2], ["C5", 0.2], ["C5", 0.2],
+    ["C5", 0.6],
+    [None, 0.4]
 ]
 
 def play(tune):
@@ -27,7 +47,7 @@ def play(tune):
 
 if __name__ == "__main__":
     try:
-        play(SONG)  # Execute the play function to start playing the tune.
+        play(MII)  # Execute the play function to start playing the tune.
 
     except KeyboardInterrupt:
         # Handle KeyboardInterrupt for graceful termination
