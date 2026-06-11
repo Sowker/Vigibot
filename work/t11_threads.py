d7b915bab179408a2a023a563525ea3760149bc1
PierreMo <pierremouries05@gmail.com>
2026-06-11 15:03:43 +0200

    t11 steer center when forward in maneuver

--- work/t11_threads.py
+++ work/t11_threads.py
@@ -214,13 +214,13 @@ def thread_controller(robot: Robot, interval: float) -> None:
             if last_turn == -1: # if we were turning left
                 if action == LinePosition.TURN_RIGHT_HARD:
                     log.info("LinePosition.TURN_RIGHT_HARD")
-                    robot.head.steer_left(STEER_HARD_DEG)
+                    robot.head.steer_center(STEER_HARD_DEG)
                     robot.motor.drive(Direction.FORWARD, SPEED_TURNING_PCT, fast_accel=True)
                     robot.state.maneuver = False
                     count_maneuver = 0
             elif last_turn == 1: # if we were turning right
                 if action == LinePosition.TURN_LEFT_HARD:
-                    robot.head.steer_right(STEER_HARD_DEG)
+                    robot.head.steer_center(STEER_HARD_DEG)
                     robot.motor.drive(Direction.FORWARD, SPEED_TURNING_PCT, fast_accel=True)
                     robot.state.maneuver = False
                     count_maneuver = 0
