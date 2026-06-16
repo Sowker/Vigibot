b5a4da466f74a733821ac7c3da368b2ab3cf4128
PierreMo <pierremouries05@gmail.com>
2026-06-16 16:57:27 +0200

    avoid objects improve scan 2

--- work/avoid_objects_threads.py
+++ work/avoid_objects_threads.py
@@ -128,13 +128,14 @@ def thread_controller(robot: Robot, interval: float) -> None:
         HR_MOTOR = 1
         VR_MOTOR = 2
         data = []
-        robot.head.set_angle_motor(VR_MOTOR, HEAD_ANGLE_CENTER)
+        robot.head.set_angle_motor(VR_MOTOR, HEAD_ANGLE_CENTER+30)
         robot.head.set_angle_motor(HR_MOTOR, HEAD_ANGLE_MAX)
+        time.sleep(0.1)
         for angle in range(HEAD_ANGLE_MAX, HEAD_ANGLE_MIN-1, -1):
             robot.head.set_angle_motor(HR_MOTOR, angle)
-            time.sleep(0.1)
+            time.sleep(0.01)
             data.append(robot.ultrasonic.read_mm())
-        robot.head.set_angle_motor(1, HEAD_ANGLE_CENTER)
+        robot.head.set_angle_motor(HR_MOTOR, HEAD_ANGLE_CENTER)
         return data
 
     while True:
