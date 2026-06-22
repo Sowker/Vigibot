f3c36b2c1db7688e50dc5c8f2847bfeb220eb043
PierreMo <pierremouries05@gmail.com>
2026-06-22 16:19:02 +0200

    removing useless

--- work/avoid_objects_threads.py
+++ work/avoid_objects_threads.py
@@ -57,82 +57,10 @@ def thread_ultrasonic(robot: Robot, interval: float) -> None:
                 break
 
         scan = scan_20_cm()
-        print("scan in ultra", scan)
 
         # time.sleep(interval)
 
     log.info("Thread arrêté")
-
-
-def thread_line(robot: Robot, interval: float) -> None:
-    """
-    Lit l'action décodée des capteurs en boucle (via read_action)
-    et met à jour directement l'action sur le RobotState.
-    """
-    log = logger.get_logger("LINE")
-    log.info("Thread démarré (intervalle=%.3f s)", interval)
-
-    while True:
-        with robot.state.lock:
-            if not robot.state.running:
-                break
-
-        # Capture matérielle et décodage atomique (Hors du Lock pour optimiser)
-        current_action = robot.line_tracker.read_action()
-
-        with robot.state.lock:
-            robot.state.line_action = current_action
-
-        time.sleep(interval)
-
-    log.info("Thread arrêté")
-
-def thread_LED(robot: Robot, interval: float):
-    """
-    Pilote les LEDs arrière (WS2812) ET les LEDs avant en parallèle :
-    - Obstacle (priorité 1) ou ligne perdue -> warning (avant + arrière)
-    - Virage gauche/droite -> clignotant correspondant (avant + arrière)
-    - Tout droit / intersection -> extinction des deux
-    """
-    log = logger.get_logger("LED")
-    log.info("Thread démarré (intervalle=%.3f s)", interval)
-
-    last_front_state = None  # 'left', 'right', 'warning' ou None
-
-    while True:
-        with robot.state.lock:
-            if not robot.state.running:
-                break
-            action    = robot.state.line_action
-            emergency = robot.state.emergency_stop
-
-        if emergency:
-            target_state = 'warning'
-            robot.led.warning()
-        elif action == LinePosition.TURN_LEFT_SOFT or action == LinePosition.TURN_LEFT_HARD:
-            target_state = 'left'
-            robot.led.clignotant_gauche()
-        elif action == LinePosition.TURN_RIGHT_SOFT or action == LinePosition.TURN_RIGHT_HARD:
-            target_state = 'right'
-            robot.led.clignotant_droit()
-        elif action == LinePosition.LINE_LOST:
-            target_state = 'warning'
-            robot.led.warning()
-        else:  # STRAIGHT / INTERSECTION
-            target_state = None
-            robot.led.arreter_clignotants()
-            robot.led.arreter_warning()
-
-        # front_leds.set_blink() est un toggle -> ne l'appeler que sur changement d'état
-        if target_state != last_front_state:
-            robot.front_leds.set_blink(target_state)
-            last_front_state = target_state
-
-        time.sleep(interval)
-
-    robot.front_leds.cancel_blink()
-    log.info("Thread arrêté")
-
 
 def thread_controller(robot: Robot, interval: float) -> None:
     """
@@ -141,26 +69,6 @@ def thread_controller(robot: Robot, interval: float) -> None:
     log  = logger.get_logger("CTRL")
     log.info("Thread démarré (intervalle=%.3f s)", interval)
     global scan
-    # def scan_180() -> list:
-    #     HR_MOTOR = 1
-    #     VR_MOTOR = 2
-    #     data = []
-    #     robot.head.set_angle_motor(VR_MOTOR, HEAD_ANGLE_CENTER+5)
-    #     robot.head.set_angle_motor(HR_MOTOR, HEAD_ANGLE_MAX)
-    #     time.sleep(0.3)
-    #     for angle in range(HEAD_ANGLE_MAX, HEAD_ANGLE_MIN-1, -1):
-    #         robot.head.set_angle_motor(HR_MOTOR, angle)
-    #         time.sleep(0.01)
-    #         data.append(robot.ultrasonic.read_mm())
-    #     time.sleep(1)
-    #     robot.head.set_angle_motor(HR_MOTOR, HEAD_ANGLE_CENTER)
-    #     return data
-    #
-    # def get_nearest_object_angle(scan : list):
-    #     min_val = min(scan)
-    #     max_id = scan.index(min_val)
-    #     nearest_object_angle = HEAD_ANGLE_MAX - max_id + HEAD_ANGLE_MIN
-    #     return nearest_object_angle
 
     while True:
         print("scan in controller", scan)
