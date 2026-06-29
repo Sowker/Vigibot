88dd19fd4d48c1b6e87241aca96eafef6b68bcdd
PierreMo <pierremouries05@gmail.com>
2026-06-29 14:59:11 +0200

    correcting servo angles for new servo

--- work/t3_servomotors.py
+++ work/t3_servomotors.py
@@ -17,9 +17,9 @@ SERVO_PCA.frequency = 50                 #
 
 
 # Limites angulaires (mécaniques)
-WHEEL_ANGLE_MIN     = 52    # degrés — braquage gauche max
-WHEEL_ANGLE_CENTER  = 90     # degrés — tout droit
-WHEEL_ANGLE_MAX     = 128    # degrés — braquage droite max
+WHEEL_ANGLE_MIN     = 55    # degrés — braquage gauche max
+WHEEL_ANGLE_CENTER  = 93     # degrés — tout droit
+WHEEL_ANGLE_MAX     = 131    # degrés — braquage droite max
 
 HEAD_ANGLE_MIN      = 10     # degrés
 HEAD_ANGLE_CENTER   = 90     # degrés
